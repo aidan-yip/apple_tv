25cebd6fc51ede8d0593d608654ccdefe20637ea
Aidan Y <jamesayipt6@gmail.com>
2024-08-13 19:13:58 -0700

    Updated content, function and design. Linked to websites

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const appstore = document.getElementById("appstore");
 const facetime = document.getElementById("facetime");
 const photos = document.getElementById("photos");
 const settings = document.getElementById("settings");
+const arcade = document.getElementById("arcade");
+const youtube = document.getElementById("youtube");
+const instagram = document.getElementById("instagram");
+const twitter = document.getElementById("twitter");
+const facebook = document.getElementById("facebook");
+const google = document.getElementById("google");
 
 appstore.onclick = () => {
   location.href = "https://www.apple.com/ca/app-store/";
@@ -16,5 +22,29 @@ photos.onclick = () => {
 };
 
 settings.onclick = () => {
-  alert("Created by Aidan Yip" + "\n" + "\n" + "Icons by " + "icons8.com");
+  location.href = "https://www.icloud.com/settings";
+};
+
+arcade.onclick = () => {
+  location.href = "https://www.apple.com/ca/apple-arcade/";
+};
+
+youtube.onclick = () => {
+  location.href = "https://www.youtube.com";
+};
+
+instagram.onclick = () => {
+  location.href = "https://www.instagram.com";
+};
+
+facebook.onclick = () => {
+  location.href = "https://www.facebook.com";
+};
+
+twitter.onclick = () => {
+  location.href = "https://x.com";
+};
+
+google.onclick = () => {
+  location.href = "https://www.google.com";
 };
